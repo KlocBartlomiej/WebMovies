c8292c30f97816aa90e0577568cc52b3a5b46d80
KlocBartlomiej <bartlomiejkloc00@gmail.com>
2024-12-04 20:41:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebMovies
+Simple app for adding movies to collection and adding comments about them 
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Hello World!</title>
+    <meta name="description" content="Lista najlepszych książek z zakresu fantastyki, science-fiction wartych przeczytania! Fantastyka naukowa - TOP Lista Wszech Czasów">
+    <meta name="keywords" content="książki, science, fiction, fantastyka, sf">
+    <meta name="author" content="Trevor Philips Industries">
+    <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
+
+    <link rel=icon” href=“favicon.ico” type=“image/x-icon”>
+    <link rel="stylesheet" href="main.css">
+    <script src="code.js"></script>
+</head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
